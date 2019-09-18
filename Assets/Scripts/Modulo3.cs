115750d7db184456502707f0639a34a6de581e19
Dion Buckley <dion-b@hotmail.com>
2019-09-18 12:27:43 +0100

    Push project to repo, have tests and impl done, NUnit up

---
+++ Assets/Scripts/Modulo3.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Modulo3
+{
+    public static int[] moduloThree(int[] x)
+    {
+        int[] result = new int[x.Length];
+        for (int i = 0; i < result.Length; i++)
+        {
+            result[i] = x[i] % 3;
+        }
+        return result;
+    }
+}
