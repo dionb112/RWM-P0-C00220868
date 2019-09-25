fd9a9de0c5bbd394b6dace50c3b6b3f12c2beba9
Dion Buckley <dion-b@hotmail.com>
2019-09-25 11:26:47 +0100

    Forgot to push final work - fully functional and well tested

--- Assets/Scripts/Modulo3.cs
+++ Assets/Scripts/Modulo3.cs
@@ -9,7 +9,20 @@ public class Modulo3
         int[] result = new int[x.Length];
         for (int i = 0; i < result.Length; i++)
         {
-            result[i] = x[i] % 3;
+            if (x[i] < 0)
+            {
+                result[i] = 3 - (-x[i] % 3);
+
+            }
+            else if (x[i] == 0)
+            {
+                result[i] = 0;
+            }
+            else
+            {
+                result[i] = x[i] % 3;
+
+            }
         }
         return result;
     }
--- Assets/Tests/Modulo3Test.cs
+++ Assets/Tests/Modulo3Test.cs
@@ -1,18 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using NUnit.Framework;
 using UnityEngine;
+using UnityEngine.TestTools;
 
-public class Modulo3Test : MonoBehaviour
+namespace Tests
 {
-    // Start is called before the first frame update
-    void Start()
+    public class Modulo3Test
     {
-        
-    }
+        [Test]
+        public void Modulo3Simple()
+        {
+            int[] input = { 7, 2, 8, 3, 9, 4, 10, 5, 11, 6 };
+            int[] output = Modulo3.moduloThree(input);
+            int[] expected = { 1, 2, 2, 0, 0, 1, 1, 2, 2, 0 };
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+            CollectionAssert.AreEqual(expected, output);
+        }
+
+        [Test]
+        public void Modulo3Complex()
+        {
+            int[] input = { 0, -2, 100000 };
+            int[] output = Modulo3.moduloThree(input);
+            int[] expected = { 0, 1, 1 };
+
+            CollectionAssert.AreEqual(expected, output);
+        }
     }
 }
